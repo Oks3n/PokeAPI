5926a642baff801ed4bcef267a90802736e74446
ExplodingHog <Victorgrud@gmail.com>
2026-04-27 21:53:40 +0200

    routing fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
 
-import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import PokedexPage from "./PokedexPage";
 import PokemonPage from "./PokemonPage";
-import AboutPage from "./AboutPage"
-import Navbar from "./Navbar"
-
+import AboutPage from "./AboutPage";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <HashRouter>
-    <Navbar />
+      <Navbar />
+
       <Routes>
+       
+        <Route path="/" element={<Navigate to="/page/1" />} />
+
+      
         <Route path="/page/:pageNumber" element={<PokedexPage />} />
-        <Route path="*" element={<PokedexPage />} />
         <Route path="/pokemon/:id" element={<PokemonPage />} />
         <Route path="/about" element={<AboutPage />} />
+
+     
+        <Route path="*" element={<Navigate to="/page/1" />} />
       </Routes>
     </HashRouter>
   );
