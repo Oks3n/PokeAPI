e9d53b57b7a80a5568806c0f9a1bd617ddae00ea
ExplodingHog <Victorgrud@gmail.com>
2026-04-27 21:37:13 +0200

    revert

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Navbar from "./Navbar"
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
     <Navbar />
       <Routes>
         <Route path="/page/:pageNumber" element={<PokedexPage />} />
@@ -20,7 +20,7 @@ function App() {
         <Route path="/pokemon/:id" element={<PokemonPage />} />
         <Route path="/about" element={<AboutPage />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
